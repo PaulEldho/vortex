6fd3660c04bf852c6c65edc7f7ce850e66d65959
PaulEldho <paulaeldho@gmail.com>
2016-03-01 13:18:02 +0530

    Update cube.py
    simple edits

--- cube.py
+++ cube.py
@@ -1,12 +1,27 @@
-import visual 
-visual.scene.width=1000
-visual.scene.height=1000
-visual.scene.center=(0,0,0)
-visual.scene.title="Rotate"
-cube = visual.box(pos=(0,0,0),axis=(0,1,0),length=0.1,width=0.1,height=0.1,color=visual.color.blue)
-#visual.box(pos=(0,0,0),axis=(0,0,1),length=0.3,width=0.2,height=0.2,color=visual.color.blue)
-while True:
-   """ visual.rate(2)
-    visual.scene.forward=visual.scene.forward.rotate(angle=0.95,axis=visual.scene.up)"""
-       
+from visual import *
+class Cube:
+    '''def __init__(self,length_box,height_box,width_box,angle_rot):
+        self.length_box=1
+        self.height_box=1
+        self.width_box=1
+        self.angle_rot=0
+        '''
+#module for box object
+    f= frame()
+    v= frame()
+    i=2
+    cube = box(frame = f,pos=(1,0,0),length=1,width=1,height=1,color=color.white)
+    def operation(length_box,height_box,width_box):
+        while i>=0:
+            """length_box=int(raw_input("enter length"))
+            height_box=int(raw_input("enter height "))
+            width_box=int(raw_input("enter width"))
+            angle_rot=int(raw_input("enter the angle in radians"))"""
+            cube.length = length_box
+            cube.width = width_box
+            cube.height = height_box
+            #cube.rotate(angle=angle_rot, axis=(0,1,0), origin=cube.pos)
+            i=i-1
+
+
 
